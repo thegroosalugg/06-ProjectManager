3caa37104731ea90e7fe3dd3783ef9eaeda4b87b
victor loginov <tkoe88@zoho.com>
2024-01-07 23:33:13 +0100

    sidebar skeleton and CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <Sidebar />
     </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <div id="sidebar">
+      <h2 className="text-2xl font-bold my-4">Projects</h2>
+      <p>sidebar content </p>
+      <p className="my-5">
+        <button>Add Project</button>
+      </p>
+    </div>
+  );
+}
