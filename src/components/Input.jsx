873148d9c408035036261a3b5e5dc7383f9712d2
victor loginov <tkoe88@zoho.com>
2024-01-08 21:04:07 +0100

    tailwind styles applied to menu

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 export default function Input({ textarea, label, ...props }) {
   return (
-    <p>
-      <label>{label}</label>
+    <p className="flex flex-col">
+      <label className="mt-3 mb-2 font-bold">{label}</label>
       {textarea ? <textarea {...props} /> : <input {...props} />}
     </p>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,13 +2,13 @@ import Input from "./Input";
 
 export default function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="flex flex-col md:w-3/5 m-3">
+      <menu className="flex gap-3 justify-end p-3 mb-3">
         <li>
-          <button>cancel</button>
+          <button>Cancel</button>
         </li>
         <li>
-          <button>save</button>
+          <button>Save</button>
         </li>
       </menu>
       <div>
