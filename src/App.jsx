3acf4e20dadae816604f28c1173863af1386b4a9
victor loginov <tkoe88@zoho.com>
2024-01-10 14:52:40 +0100

    project form now dynamicaly appears when button clicked

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import NewProject from "./components/NewProject";
 
 function App() {
   const [projects, setProjects] = useState([]);
+  const [displayForm, setDisplay] = useState(false)
 
   function handleAddProject(newProject) {
     setProjects(prevProjects => [...prevProjects, newProject]);
   }
 
+  function handleDisplay() {
+    setDisplay(true)
+  }
+
   console.log(projects);
+  console.log(displayForm);
 
   return (
     <main className="h-screen my-2 md:my-8 flex gap-2 md:gap-8">
     {/* <main className="h-screen my-2 md:my-8 flex flex-col md:flex-row gap-8"> */}
-      <Sidebar projects={projects} />
-      <NewProject addProject={handleAddProject} />
+      <Sidebar projects={projects} addProject={handleDisplay} />
+      {displayForm && <NewProject addProject={handleAddProject} />}
     </main>
   );
 }
