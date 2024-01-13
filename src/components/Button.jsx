677e918e1d0b3c6160bc7aa6e141b17b87e6ecf4
victor loginov <tkoe88@zoho.com>
2024-01-13 15:37:40 +0100

    updated component and styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,11 @@
 export default function Button({ name, handleClick }) {
   return (
-    <li>
       <button
-        className="font-semibold p-3 rounded-xl hover:bg-stone-900 hover:text-stone-50"
+        className="font-semibold py-2 rounded-xl hover:bg-stone-900 hover:text-stone-50"
         style={{ minWidth: "5rem" }}
         onClick={handleClick}
       >
         {name}
       </button>
-    </li>
   );
 }
--- src/components/ProjectView.jsx
+++ src/components/ProjectView.jsx
@@ -1,22 +1,20 @@
+import Button from "./Button";
+
 export default function ProjectView({ name, desc, date }) {
   return (
-    <div className="my-20 md:w-3/5 m-3">
-      <span className="flex justify-between">
+    <div className="my-20 w-3/5 m-3">
+      <div className="flex justify-between">
         <h2 className="font-bold text-2xl uppercase">{name}</h2>
-        <button className="font-semibold py-1 px-2 rounded-xl hover:bg-stone-900 hover:text-stone-50">
-          Delete
-        </button>
-      </span>
-      <p className="mb-1">{date}</p>
-      <p className="overflow-auto break-all my-2 max-h-20">{desc}</p>
+        <Button name={'Delete'} />
+      </div>
+      <p className="mb-1 text-stone-600">{date}</p>
+      <p className="overflow-auto break-all my-2 max-h-40">{desc}</p>
       <hr className="my-4 border-t-2 border-gray-500" />
-      <h2>Tasks</h2>
-      <span>
-        <input type="text" />
-        <button className="font-semibold py-1 px-2 rounded-xl hover:bg-stone-900 hover:text-stone-50 ml-3">
-          Add Task
-        </button>
-      </span>
+      <h2 className="font-bold my-3 text-2xl">Tasks</h2>
+      <div className="flex items-center gap-4">
+        <input type="text" className="border bg-stone-200 my-3 mr-3" />
+        <Button name={'Add Task'} />
+      </div>
       <ol className="overflow-auto max-h-50">
         <li>Task</li>
       </ol>
